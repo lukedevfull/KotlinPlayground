dc65e3b5b652be5324bffd0717e4ae06f988e9d7
LukeDevFull <contato.prof2@gmail.com>
2026-09-14 17:12:42 -0300

    feat: treinando o basico

---
+++ src/BasicConcepts/Aritimetc.kt
@@ -0,0 +1,21 @@
+package BasicConcepts
+
+fun main() {
+    val soma = 2 + 2
+    val subtracao = 2 - 2
+    val multiplicacao = 2 * 10
+    val divisao = 100 / 5
+    val resto = 10 % 3
+
+    var valor = 0
+    // logica
+    // logica
+    // logica
+    // logica
+
+//    valor = valor + 15
+    valor += 15
+
+    valor++
+    valor--
+}
---
+++ src/BasicConcepts/StringFomatation.kt
@@ -0,0 +1,10 @@
+package BasicConcepts
+
+fun main() {
+    val linguagem = "Kotlin"
+    val caracteristica = "é show!"
+
+    println("$linguagem $caracteristica")
+
+    println("Double max value é: ${Double.MAX_VALUE}.")
+}
