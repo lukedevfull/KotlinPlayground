7900524355d22ecd3422b58abb7dfe61cf5367da
LukeDevFull <contato.prof2@gmail.com>
2026-09-22 15:00:16 -0300

    feat: treinando o basico

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main(){
-    print("Ola Mundo")
+fun helloWorld(nome: String) = println("Olá, $nome!")
+
+fun media(n1: Int, n2: Int): Int = (n1 + n2) / 2
+fun soma(n1: Int, n2: Int) = n1 + n2
+
+fun main() {
+    helloWorld("Luke")
+    println(media(10, 8))
+    println(soma(10, 85))
 }
